3fd82c24cbbd06e3263a9c895599fedafa40e48b
JANENDRA SINGH <110004964+Janendrasingh@users.noreply.github.com>
2026-04-13 23:09:49 +0530

    Implement button animation on sound play
    Added button animation for drum buttons and key presses.

--- event.js
+++ event.js
@@ -9,6 +9,8 @@ for (var i = 0; i < numberOfDrumButton; i++) {
         var buttonInnerHTML = this.innerHTML;
 
         makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML)
     });
 }
 
@@ -16,6 +18,8 @@ for (var i = 0; i < numberOfDrumButton; i++) {
 
 document.addEventListener("keypress", function(event){
     makeSound(event.key);
+
+    buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -53,7 +57,12 @@ function makeSound(key){
 }
 
 
-
-
+function buttonAnimation(currentKey){
+        var activeButton = document.querySelector("." + currentKey);
+        activeButton.classList.add("pressed");
+        setTimeout(function(){
+                activeButton.classList.remove("pressed");
+        }, 100);
+}
 
 
